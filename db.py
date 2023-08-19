82f37fbe14fb44be7545fac03f16a35e8967a016
Kar_Yan <102352076+Karlenjak@users.noreply.github.com>
2023-08-19 18:29:56 +0200

    change remove files

--- db.py
+++ db.py
@@ -1,10 +1,20 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_database = os.getenv("DB_DATABASE")
 
 db = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='Karlen-1999',
-    database='contactos'
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_database
 )
 
 def rellenar_datos(nom, email, ident, area):
